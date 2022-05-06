5799aca9954ee1d3c3dca8d1ce4342debb3e9801
irinainina <irbris@mail.ru>
2022-05-06 08:44:28 +0300

    Updates

---
+++ precache-manifest.01dacd6c941ea153a5eb84906c6f5bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90a2db819cd0fe4ac582c6895361bf0",
+    "url": "/halolab/index.html"
+  },
+  {
+    "revision": "5db694004a9cbc3aca8a",
+    "url": "/halolab/static/css/main.7eabf340.chunk.css"
+  },
+  {
+    "revision": "a5ce9c0d28ef624633f2",
+    "url": "/halolab/static/js/2.65f6b363.chunk.js"
+  },
+  {
+    "revision": "5db694004a9cbc3aca8a",
+    "url": "/halolab/static/js/main.61eee972.chunk.js"
+  },
+  {
+    "revision": "f3db210a6a101a5351df",
+    "url": "/halolab/static/js/runtime~main.b19ef9aa.js"
+  },
+  {
+    "revision": "3f0c69fa996f23e1fc42b1f08e9fe94d",
+    "url": "/halolab/static/media/arrow-right.3f0c69fa.svg"
+  },
+  {
+    "revision": "358bbcefb54a7ea4ebf37bed7c80eaeb",
+    "url": "/halolab/static/media/error.358bbcef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/holalab/precache-manifest.b791ce5db9b853d3d256b2b821856600.js"
+  "/halolab/precache-manifest.01dacd6c941ea153a5eb84906c6f5bc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/holalab/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/halolab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
